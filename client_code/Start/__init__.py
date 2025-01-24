cae4a6f4cc2d9a20a7c4e962f3422bbb3678e9a6
walternzd@gmail.com <walternzd@gmail.com>
2025-01-24 08:38:31 +0000

    Created ServerModule1; Edited settings and Start; Deleted Form1

--- client_code/Start/__init__.py
+++ client_code/Start/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import StartTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.facebook.auth
@@ -16,3 +17,11 @@ class Start(StartTemplate):
     anvil.users.login_with_form()
 
     # Any code you write here will run before the form opens.
+  def logout_btn_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    anvil.users.logout()
+    anvil.users.login_with_form()
+
+  def subscriptionSubmit_btn_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    pass
