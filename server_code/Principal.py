3bdda5ff1c32a87a8b8d546d191fafd089ce136b
Walter462 <74890413+Walter462@users.noreply.github.com>
2025-02-05 15:30:05 +0800

    refactor: add EventTimeSchedule to calculate effective dates measures

--- server_code/Principal.py
+++ server_code/Principal.py
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from datetime import datetime as dt
-from typing import Union, Literal, Optional
+from typing import Union, Literal, Optional, Tuple
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -24,22 +24,33 @@ from typing import Union, Literal, Optional
 class Loan:
 	def __init__(self, loan_id):
 		self.loan_id = loan_id 			#assigned by the database
-		self.currency = Currency()
-		
+		self.loan_currency = Currency()
+		self.loan_events_list = []
+		self.sorted_loan_events_list = []
+
+	def loan_events_list(self, events):
+		self.loan_events_list = [event for event in events if event.loan_id == self.loan_id]
+
+	def sorted_loan_events_list(self, loan_events_list):
+		self.sorted_loan_events_list = sorted(loan_events_list, key=lambda x: x.event_date)
+
+
+class EventTimeSchedule:
+		def __init__(self):
+			self.event_beggining_date:	Optional[dt.date]
+			self.event_ending_date:			Optional[dt.date]
+			@property
+			def event_days_count(self):
+				return (self.event_ending_date - self.event_beggining_date).days
+
 class EventParams:
-	def __init__(self, 
-							date: Optional[dt.date] = None, 
-							time: Optional[dt.time] = None
-							):
-		self.loan_id:			Union[int, None] = 0	#fetch from the database	
-		self.event_id:		Union[int, None] = 0	#assigned by the database
-		self.event_date:	Optional[dt.date]= date if date else dt.now().date()
-		self.event_time:	Optional[dt.timetz] = time if time else dt.now().timetz()
-		self.event_beggining_date:	Optional[dt.date] = date if date else dt.now().date()
-		self.event_ending_date:			Optional[dt.date] = date if date else dt.now().date()
-		@property
-		def event_days(self):
-			return (self.event_ending_date - self.event_beggining_date).days
+	def __init__(self, date=None, time=None):
+		self.loan_id:			Union[int, None] = 0	#Fetch from DB
+		self.event_id:		Union[int, None] = 0	#Assigned by DB
+		self.event_date:	Optional[dt.date]= date or dt.now().date()
+		self.event_time:	Optional[dt.timetz] = time or dt.now().timetz()
+
+		self.event_type:	Literal["User input", "Automated"] = None
 		self.event_name:	Optional[Literal["Principal lending", 
 																		"Repayment", 
 																		"Interest rate change",
@@ -47,28 +58,27 @@ class EventParams:
 																		"Principal balance correction",
 																		"Interest balance correction",
 																		"Leap year switch"]] = None
-		self.event_type:	Literal["User input", "Automated"] = None
 
 class Currency:
-	def __init__(self, 
-				ticker = None, 
-				name = None, 
-				rate = None, 
-				currency_sum = 0):
-		self.name = "Great Britain pounds"
-		self.ticker = "GBP"
-		self.rate = 1
-		self.sum = currency_sum
+    def __init__(self, 
+                 ticker="GBP", 
+                 name="Great Britain pounds", 
+                 rate=1, 
+                 currency_sum=0):
+        self.name = name
+        self.ticker = ticker
+        self.rate = rate
+        self.currency_sum = currency_sum
+
 		
 class PrincipalLending(EventParams):
-	def __init__(self, principal_lending_sum = None):
+	def __init__(self, principal_lending_currency_sum = None):
 		super().__init__()
-		self.currency = Currency(currency_sum = principal_lending_sum)
+		self.principal_lending_currency_sum = Currency(currency_sum = principal_lending_currency_sum)
 		self.event_name = "Principal lending"
 		self.event_type = "User input"
-		self.principal_lending_sum = 0
 	@property
-	def sum(self):
+	def principal_lending_sum(self): #convert to loan_currency
 		return self.currency.rate * self.currency.sum
 
 class Repayment(EventParams):
@@ -78,5 +88,5 @@ class Repayment(EventParams):
 		self.event_name = "Repayment"
 		self.event_type = "User input"
 
-Lending_1 = PrincipalLending(principal_lending_sum = 300000)
+Lending_1 = PrincipalLending(principal_lending_currency_sum = 300000)
 print(Lending_1.event_type)
