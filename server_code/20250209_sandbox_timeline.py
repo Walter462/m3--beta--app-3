a22a9b148768766fdfc694e95c12d929bbf93f2f
Walter462 <74890413+Walter462@users.noreply.github.com>
2025-02-11 14:15:59 +0800

    FIX: Currency values population error(None)

--- server_code/20250209_sandbox_timeline.py
+++ server_code/20250209_sandbox_timeline.py
@@ -11,34 +11,40 @@ dict5 = {'date':'2024-01-03', 'principal_lending':333, 'Currency':'USD'} #duplic
 dict4 = {'date':'2023-02-01', 'principal_lending':66, 'Currency':'USD'}
 dict6 = {'date':'2024-01-04', 'repayment': 113, 'Currency':'USD'}
 events_list_raw.extend([dict1, dict2, dict3, dict4, dict5, dict6])
-#print (*events_list_raw, sep = "\n")
-#print('---')
-events_list_raw_sorted = sorted(events_list_raw, key=lambda item: item['date'])
-print (*events_list_raw_sorted, sep = "\n")
-print('---')
+#events_list_raw_sorted = sorted(events_list_raw, key=lambda item: item['date'])
+
 #Create deafault data structure
 default_dict = defaultdict(lambda: {'date':None, 
                                   'principal_lending': 0, 
-                                  'capitalization': 0,
-                                  'principal_balance_correction': 0,
                                   'Currency': None, 
-                                  'repayment':0})
+                                  'capitalization': 0,
+                                  'repayment':0,
+                                  'principal_balance_correction': 0,
+                                  'principal_balance': 0
+                                  })
+
 #Aggregate transactions by date: sum values for the same date
 for item in events_list_raw:
   date_key = datetime.strptime(item['date'], "%Y-%m-%d")
   default_dict[date_key]['principal_lending'] += item.get('principal_lending', 0) #helps to aggregate events at the same date
   default_dict[date_key]['repayment'] += item.get('repayment', 0)
+  default_dict[date_key]['Currency'] = item.get('Currency', None)
   default_dict[date_key]['date'] = date_key
+
 events_list_date_aggregated_sorted = sorted(default_dict.values(), key=lambda item: item['date'])
 print(*events_list_date_aggregated_sorted, sep = "\n")
 print('---')
 
 principal_balance = 0
 for item in events_list_date_aggregated_sorted:
-  principal_balance +=item['principal_lending']+item['capitalization']+item['principal_balance_correction']-item['repayment']
+  principal_balance +=item['principal_lending']+item['capitalization']-item['repayment']+item['principal_balance_correction']
   item['principal_balance'] = principal_balance
 
-print(*events_list_date_aggregated_sorted, sep = "\n")
+# Final dataset
+print(*[f"{item['date']} - {item['principal_balance']}" for item in events_list_date_aggregated_sorted], sep="\n")
+print('---')
+
+#Analytical section
 def balance_report(data, report_date):
   report_date = datetime.strptime(report_date, "%Y-%m-%d")
   if report_date < data[0]['date']:
@@ -54,5 +60,5 @@ def balance_report(data, report_date):
   # If report_date is after all transactions, return last principal_balance
   return previous_balance
 
-print(balance_report(events_list_date_aggregated_sorted,'2024-01-03'))
 
+print(balance_report(events_list_date_aggregated_sorted,'2024-01-03'))
